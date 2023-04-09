bbfc80086f40ac52c72dc62d9ef80e3f2866aead
melon.li <lijunwork95@gmail.com>
2023-04-09 14:45:18 +0800

    feat: useUnmountedRef

---
+++ src/useUnmountedRef/index.ts
@@ -0,0 +1,13 @@
+import { MutableRefObject, useRef } from "react";
+import useUnmount from "../useUnmount";
+
+const useUnmountedRef = (): MutableRefObject<boolean> => {
+  const unmountedRef = useRef<boolean>(false);
+
+  useUnmount(() => {
+    unmountedRef.current = true;
+  })
+  return unmountedRef
+}
+
+export default useUnmountedRef;
---
+++ src/useUnmountedRef/tests/index.test.ts
@@ -0,0 +1,13 @@
+import { renderHook } from "@testing-library/react";
+import useUnmountedRef from "../";
+
+describe("useUnmountedRef", () => {
+  it("test useUnmountedRef", () => {
+    const hook = renderHook(() => useUnmountedRef());
+    expect(hook.result.current.current).toBe(false);
+    hook.rerender()
+    expect(hook.result.current.current).toBe(false);
+    hook.unmount()
+    expect(hook.result.current.current).toBe(true);
+  })
+});
